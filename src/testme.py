3ab15965be0836dc252355fd10c797dc8ae4191c
Baptiste Covolato <b.covolato@gmail.com>
2013-07-18 18:10:03 +0200

    Start redesign of the soft

--- src/testme.py
+++ src/testme.py
@@ -1,4 +1,4 @@
-#!env /usr/bin/python2
+#!env /usr/bin/python3
 
 #Copyright (C) 2013 Baptiste COVOLATO <b.covolato@gmail.com>
 #
@@ -23,26 +23,109 @@
 import sys
 import os
 import re
-import ConfigParser
+import configparser
 import filecmp
 import argparse
 
-testme_version = "0.1"
+class TestPrinter:
+    def __init__(self):
+        self.verbose = False
+        self.summary_on = True
+
+    def print_summary(self, category, good, total):
+        if self.summary_on:
+            print("[TESTME] Category : '" + category + "' :", good, "/", total)
+
+    def print_verbose(self, message):
+        if self.verbose:
+            print(message)
+
+    def print_error(self, message):
+        print("\033[91m" + message + "\033[0m")
+
+class TestSuit:
+    def __init__(self, printer):
+        self.environement = {}
+        self.test_list = {}
+        self.running_cat = ""
+        self.running_dir = ""
+        self.ext = ""
+        self.total_test = 0
+        self.total_good = 0
+        self.cat_test = 0
+        self.cat_good = 0
+        self.printer = printer
+
+    def expand(self, string):
+        while 1:
+            m = re.search('\$\{(.*?)\}', string)
+
+            if m == None or self.environement.get(m.group(1), None) == None:
+                break
+
+            string = re.sub('\$\{' + m.group(1) + '\}', self.environement[m.group(1)], string)
+
+        return string
+
+    def cat_field_get(self, field):
+        if isinstance(self.test_list[self.running_cat][field], str):
+            return self.expand(self.test_list[self.running_cat][field])
+        else:
+            return self.test_list[self.running_cat][field]
+
+    def is_runnable(self):
+        if testme_args.get('category', None) != None:
+            return testme_args['category'].count(self.running_cat) > 0
+
+        return True
+
+    def run_directory(self):
+        try:
+            for dir_file in os.listdir(self.cat_field_get(self.running_dir)):
+                if dir_file.endswith(self.cat_field_get(self.ext)):
+                    self.total_test += 1
+                    self.cat_test += 1
+                    test_result = self.run_test(category, files)
+                    if test_result:
+                        self.cat_good += 1
+                        self.total_good += 1
+                    self.printer.print_result(test_result, category, files)
+
+            self.printer.print_summary(self.running_cat, self.total_test,
+                                       self.cat_test)
+
+        except OSError:
+           self.printer.print_error("[TESTME] Fatal error with " +
+                                    "directory : " +
+                                    self.cat_field_get(self.running_dir))
+
+    def run(self):
+        self.total_test = 0
+        self.total_good = 0
+
+        # Run every categories
+        for cat in self.test_list:
+            self.cat_test = 0
+            self.cat_good = 0
+            self.running_cat = cat
+
+            if not self.is_runnable():
+                continue
+
+            print("\n[TESTME] Running tests in", self.running_cat)
+            self.running_dir = 'input_dir' if self.cat_field_get('input') else 'stdin_dir'
+            self.ext = 'input_ext' if self.cat_field_get('input') else 'stdin_ext'
+
+            self.run_directory()
+
+        print("\n[TESTME] Final result :", self.total_good, "/", self.total_test)
+
+testme_version = "0.2"
 testme_config_name = "testme.conf"
 testme_args = {}
 testme_to_run = {}
 testme_env = {}
 
-def parse_string(string):
-    m = re.search('\$\{(.*)\}', string)
-
-    if m == None:
-        return string
-
-    if testme_env.get(m.group(1), None) == None:
-        return string
-
-    return re.sub('\$\{(.*)\}', testme_env[m.group(1)], string)
 
 def update_env(cat):
     global testme_env
@@ -57,7 +140,7 @@ def change_extension(change_file, ext):
 
 def print_verbose(msg):
     if testme_args.get('verbose', None):
-        print msg
+        print(msg)
 
 def run(category, test_file):
     global testme_env
@@ -109,139 +192,101 @@ def print_result(test_result, category, file_name):
     if (test_result and (testme_to_run[category]['display_ok_tests']
                         or testme_args.get('full_display', False))
                    and not testme_args.get('extra_light_display', False)):
-        print "\033[32m[TESTME] Test :", file_name, "passed\033[0m"
+        print("\033[32m[TESTME] Test :", file_name, "passed\033[0m")
     elif (not test_result and (testme_to_run[category]['display_ko_tests']
                                or testme_args.get('full_display', False)
                                or testme_args.get('light_display', False))
                           and not testme_args.get('extra_light_display', False)):
-        print "\033[91m[TESTME] Test :", file_name, "failed\033[0m"
+        print("\033[91m[TESTME] Test :", file_name, "failed\033[0m")
 
-def is_runnable_category(cat_name):
-    if testme_args.get('category', None) != None:
-        return testme_args['category'].count(cat_name) > 0
-
-    return True
-
-def run_tests():
-    total_test = 0
-    total_pass = 0
-    for category in testme_to_run:
-        cat_test = 0
-        cat_pass = 0
-
-        if not is_runnable_category(category):
-            continue
-        print "\n[TESTME] Running tests in", category
-        dir_to_run = 'input_dir' if testme_to_run[category]['input'] else 'stdin_dir'
-        ext = 'input_ext' if testme_to_run[category]['input'] else 'stdin_ext'
-
-        try:
-            for files in os.listdir(testme_to_run[category][dir_to_run]):
-                if files.endswith(testme_to_run[category][ext]):
-                    total_test += 1
-                    cat_test += 1
-                    test_result = run(category, files)
-                    if test_result:
-                        cat_pass += 1
-                        total_pass += 1
-                    print_result(test_result, category, files)
-
-            if testme_to_run[category]['display_summary'] == 1:
-                print "[TESTME] Category : '" + category + "' :", cat_pass, "/", cat_test
-
-        except OSError:
-            sys.stderr.write("[TESTME][ERROR] Fatal error with directory : " +
-                             testme_to_run[category][dir_to_run] + "\n")
-
-
-    print "\n[TESTME] Final result :", total_pass, "/", total_test
 
 def die_on_error(msg):
     sys.stderr.write("[TESTME][ERROR] " + msg + "\n")
     sys.exit(1)
 
-def default_category(cat_name):
-    return {'stdout' : 0, # Stdout test
-            'stdin' : 0, # Stdin test
-            'stderr' : 0, # Stderr test
-            'input' : 0, # Input test
-            'input_dir' : os.path.join(testme_args['dir'], cat_name, "input"),
-            'stdout_dir' : os.path.join(testme_args['dir'], cat_name, "stdout"),
-            'stdin_dir' : os.path.join(testme_args['dir'], cat_name, "stdin"),
-            'stderr_dir' : os.path.join(testme_args['dir'], cat_name, "stderr"),
-            'input_ext' : 'inp',
-            'stdout_ext' : 'out',
-            'stdin_ext' : 'in',
-            'stderr_ext' : 'err',
-            'cmd_line' : '',
-            'check_code' : 0,
-            'error_code' : ["0"],
-            'display_ok_tests' : 1,
-            'display_ko_tests' : 1,
-            'display_summary' : 1}
 
-def get_bool(config, dico, section, value):
-    try:
-        dico[section][value] = config.getboolean(section, value)
-    except ConfigParser.NoOptionError:
-        return 0
-    except:
-        die_on_error(value + " in section '" + section + "' is not a boolean")
+class ConfigBuilder:
+    def __init__(self, printer, filename):
+        self.filename = filename
+        self.configuration = {}
+        self.printer = printer
 
-def get_string(config, dico, section, value):
-    try:
-        txt = config.get(section, value)
-        dico[section][value] = parse_string(txt)
-        return
-    except ConfigParser.NoOptionError:
-        return
+    def default(self, cat_name):
+        return {'stdout' : 0, # Stdout test
+                'stdin' : 0, # Stdin test
+                'stderr' : 0, # Stderr test
+                'input' : 0, # Input test
+                'input_dir' : os.path.join(testme_args['dir'], cat_name, "input"),
+                'stdout_dir' : os.path.join(testme_args['dir'], cat_name, "stdout"),
+                'stdin_dir' : os.path.join(testme_args['dir'], cat_name, "stdin"),
+                'stderr_dir' : os.path.join(testme_args['dir'], cat_name, "stderr"),
+                'input_ext' : 'inp',
+                'stdout_ext' : 'out',
+                'stdin_ext' : 'in',
+                'stderr_ext' : 'err',
+                'cmd_line' : '',
+                'check_code' : 0,
+                'error_code' : ["0"],
+                'display_ok_tests' : 1,
+                'display_ko_tests' : 1,
+                'display_summary' : 1}
 
-def get_int(config, dico, section, value):
-    try:
-        dico[section][value] = config.getint(section, value)
-    except ConfigParser.NoOptionError:
-        return 0
-    except:
-        die_on_error(value + " in section '" + section + "' is not an integer")
+    def get_bool(self, section, value):
+        try:
+            self.configuration[section][value] = self.parser.getboolean(section,
+                                                                        value)
+        except configparser.NoOptionError:
+            return 0
+        except:
+            die_on_error(value + " in section '" + section + "' is not a boolean")
 
-def parse_config():
-    global testme_to_run
+    def get_string(self, section, value):
+        try:
+            self.configuration[section][value] = self.parser.get(section, value)
+            return
+        except configparser.NoOptionError:
+            return
 
-    config_full_path = os.path.join(testme_args['dir'], testme_config_name)
-    print_verbose("[TESTME] Parsing configuration file : " + config_full_path)
+    def get_int(self, section, value):
+        try:
+            self.configuration[section][value] = self.parser.getint(section, value)
+        except configparser.NoOptionError:
+            return 0
+        except:
+            die_on_error(value + " in section '" + section + "' is not an integer")
 
-    config_file = ConfigParser.RawConfigParser()
-    config_file.read(config_full_path)
+    def parse_config(self):
+        self.parser = configparser.RawConfigParser()
+        self.parser.read(self.filename)
 
-    for section in config_file.sections():
-        testme_to_run[section] = default_category(section)
-        update_env(section)
+        for section in self.parser.sections():
+            self.configuration[section] = self.default(section)
 
-        get_bool(config_file, testme_to_run, section, 'stdout')
-        get_bool(config_file, testme_to_run, section, 'stdin')
-        get_bool(config_file, testme_to_run, section, 'stderr')
-        get_bool(config_file, testme_to_run, section, 'input')
-        get_string(config_file, testme_to_run, section, 'stdout_dir')
-        get_string(config_file, testme_to_run, section, 'stdin_dir')
-        get_string(config_file, testme_to_run, section, 'stderr_dir')
-        get_string(config_file, testme_to_run, section, 'input_dir')
-        get_string(config_file, testme_to_run, section, 'cmd_line')
-        get_string(config_file, testme_to_run, section, 'input_ext')
-        get_string(config_file, testme_to_run, section, 'stdin_ext')
-        get_string(config_file, testme_to_run, section, 'stdout_ext')
-        get_string(config_file, testme_to_run, section, 'stderr_ext')
-        get_string(config_file, testme_to_run, section, 'cmd_line')
-        get_bool(config_file, testme_to_run, section, 'check_code')
-        get_string(config_file, testme_to_run, section, 'error_code')
-        get_bool(config_file, testme_to_run, section, 'display_ok_tests')
-        get_bool(config_file, testme_to_run, section, 'diplay_ko_tests')
-        get_bool(config_file, testme_to_run, section, 'display_summary')
+            self.get_bool(section, 'stdout')
+            self.get_bool(section, 'stdin')
+            self.get_bool(section, 'stderr')
+            self.get_bool(section, 'input')
+            self.get_string(section, 'stdout_dir')
+            self.get_string(section, 'stdin_dir')
+            self.get_string(section, 'stderr_dir')
+            self.get_string(section, 'input_dir')
+            self.get_string(section, 'cmd_line')
+            self.get_string(section, 'input_ext')
+            self.get_string(section, 'stdin_ext')
+            self.get_string(section, 'stdout_ext')
+            self.get_string(section, 'stderr_ext')
+            self.get_string(section, 'cmd_line')
+            self.get_bool(section, 'check_code')
+            self.get_string(section, 'error_code')
+            self.get_bool(section, 'display_ok_tests')
+            self.get_bool(section, 'diplay_ko_tests')
+            self.get_bool(section, 'display_summary')
 
-        if testme_to_run[section]['error_code'] is str:
-            testme_to_run[section]['error_code'] = testme_to_run['error_code'].split("|")
+            if self.configuration[section]['error_code'] is str:
+                self.configuration[section]['error_code'] = self.configuration[section]['error_code'].split("|")
 
 def parse_argv():
-    global testme_running_dir, testme_verbose, testme_args
+    global testme_args
     argc = 1
 
     if len (sys.argv) == 1:
@@ -266,13 +311,23 @@ def parse_argv():
     testme_args = vars(parser.parse_args())
 
 def main():
+    printer = TestPrinter()
     parse_argv()
+
+    printer.verbose = testme_args.get('verbose', None)
 
     if testme_args.get('dir', None) == None:
         testme_args['dir'] = os.getcwd()
 
-    print_verbose("[TESTME] Running directory : " + testme_args['dir'])
-    parse_config()
-    run_tests()
+    printer.print_verbose("[TESTME] Running directory : " + testme_args['dir'])
+    config_full_path = os.path.join(testme_args['dir'], testme_config_name)
 
-main()
+    config = ConfigBuilder(printer, config_full_path)
+    config.parse_config()
+
+    test_suit = TestSuit(printer)
+    test_suit.test_list = config.configuration
+    test_suit.run()
+
+if __name__ == "__main__":
+    main()
